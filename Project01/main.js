01297c87db4fcca60d0c9853ae63c08ee2d405ad
SahilKumarLok <sk8579112@gmail.com>
2024-02-24 11:41:13 +0500

    addition

--- Project01/main.js
+++ Project01/main.js
@@ -1,4 +1,8 @@
-var message = "Hello World";
+"use strict";
+let message = "Hello World";
 //message = "abc";
 console.log(message);
 // let firstName = "Sahil Kumar";
+let a = "1";
+let b = "1";
+console.log("a+b");
--- Project01/main.ts
+++ Project01/main.ts
@@ -3,3 +3,8 @@ let message = "Hello World";
 console.log(message);
 
 // let firstName = "Sahil Kumar";
+
+let a = "1";
+let b = "1"
+console.log("a+b");
+
